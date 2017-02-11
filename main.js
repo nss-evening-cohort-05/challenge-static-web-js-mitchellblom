bdd728207111c1fba23f727d0071c5010cf0af82
Mitchell Blom <mitchellblom@gmail.com>
2017-02-10 22:12:41 -0600

    logging user inputs on button click

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-var userHeight = document.getElementById("inputHeight").value;
-var userChar = document.getElementById("inputCharacter");
 var button = document.getElementById("showMe");
 var treeStack = "";
 
 button.addEventListener("click", showMe);
 
 function showMe (clickEvent) {
-	for (i = 0; i < userHeight; i++) {
-  	treeStack += userChar;
-  	console.log(treeStack);
-  } 
+	var userHeight = document.getElementById("inputHeight").value;
+	var userChar = document.getElementById("inputCharacter").value;
+	console.log(userHeight);
+	console.log(userChar);
 }
 
-showMe(treeStack);
+// function showMe (clickEvent) {
+// 	for (i = 0; i < userHeight; i++) {
+//   	treeStack += userChar;
+//   	console.log(treeStack);
+//   } 
+// }
+
+// showMe(userChar);
+
+
+
+
+
+
+
+// function inputKeyUp(e) {
+//     e.which = e.which || e.keyCode;
+//     if(e.which == 13) {
+//         // submit
+//     }
+// }
